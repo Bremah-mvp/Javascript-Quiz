a7bd44781a0fa883ebacb194b5d63f69e299db80
jackie <mvpbremah@yahoo.com>
2020-07-19 14:17:12 -0400

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Gathering HTML elements for manipulation
+// values evaluated
 var quizBody = document.getElementById("quiz");
 var resultsEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("finalScore");
@@ -26,40 +26,43 @@ var quizQuestions = [{
     choiceB: "National Basketball Association",
     choiceC: "New basketball association",
     choiceD: "Netflix best adds",
-    correctAnswer: "b"},
-  {
+    correctAnswer: "b"
+},
+{
     question: "Who won the first ever NBA championship?",
     choiceA: "Golden state worriors",
     choiceB: "Lakers",
     choiceC: "Chicago bulls",
     choiceD: "Boston Celtics",
-    correctAnswer: "d"},
-   {
+    correctAnswer: "d"
+},
+{
     question: "Which NBA franchise has won the most championships?",
     choiceA: "Chicago bulls",
     choiceB: "Denver nuggets",
     choiceC: "Boston celtics",
     choiceD: "Portland Trail blazers",
-    correctAnswer: "c"},
-    {
+    correctAnswer: "c"
+},
+{
     question: "What NBA team did Micheal Jordan play for?",
     choiceA: "Sixers",
     choiceB: "Miami Heat",
     choiceC: "Dallas mavericks",
     choiceD: "chicago Bulls",
-    correctAnswer: "d"},
-    {
+    correctAnswer: "d"
+},
+{
     question: "What NBA team does Stephen Curry play for?",
     choiceA: "Raptors",
     choiceB: "Golden state warriors",
     choiceC: "San Antonio Spurs",
     choiceD: "Houston rockets",
-    correctAnswer: "b"},  
-   
-        
-    
-    ];
-// Other global variables
+    correctAnswer: "b"
+},
+
+];
+
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
 var timeLeft = 50;
@@ -67,12 +70,12 @@ var timerInterval;
 var score = 0;
 var correct;
 
-// This function cycles through the object array containing the quiz questions to generate the questions and answers.
-function generateQuizQuestion(){
+//  generate questions and answers.
+function generateQuizQuestion() {
     gameoverDiv.style.display = "game over";
-    if (currentQuestionIndex === finalQuestionIndex){
+    if (currentQuestionIndex === finalQuestionIndex) {
         return showScore();
-    } 
+    }
     var currentQuestion = quizQuestions[currentQuestionIndex];
     questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
     buttonA.innerHTML = currentQuestion.choiceA;
@@ -81,26 +84,26 @@ function generateQuizQuestion(){
     buttonD.innerHTML = currentQuestion.choiceD;
 };
 
-// Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
-function startQuiz(){
+//  first quiz question.
+function startQuiz() {
     gameoverDiv.style.display = "none";
     startQuizDiv.style.display = "none";
     generateQuizQuestion();
 
-    //Timer
-    timerInterval = setInterval(function() {
+    
+    timerInterval = setInterval(function () {
         timeLeft--;
         quizTimer.textContent = "Time left: " + timeLeft;
-    
-        if(timeLeft === 0) {
-          clearInterval(timerInterval);
-          showScore();
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            showScore();
         }
-      }, 1000);
+    }, 1000);
     quizBody.style.display = "block";
 }
-// This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
-function showScore(){
+// dispaly score 
+function showScore() {
     quizBody.style.display = "none"
     gameoverDiv.style.display = "flex";
     clearInterval(timerInterval);
@@ -108,41 +111,40 @@ function showScore(){
     finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
 }
 
-// On click of the submit button, we run the function highscore that saves and stringifies the array of high scores already saved in local stoage
-// as well as pushing the new user name and score into the array we are saving in local storage. Then it runs the function to show high scores.
-submitScoreBtn.addEventListener("click", function highscore(){
-    
-    
-    if(highscoreInputName.value === "") {
+// submit score
+submitScoreBtn.addEventListener("click", function highscore() {
+
+// initials cant be empty alert
+    if (highscoreInputName.value === "") {
         alert("enter desired initials");
         return false;
-    }else{
+    } else {
         var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
         var currentUser = highscoreInputName.value.trim();
         var currentHighscore = {
-            name : currentUser,
-            score : score
+            name: currentUser,
+            score: score
         };
-    
+
         gameoverDiv.style.display = "none";
         highscoreContainer.style.display = "flex";
         highscoreDiv.style.display = "block";
         endGameBtns.style.display = "flex";
-        
+
         savedHighscores.push(currentHighscore);
         localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
         generateHighscores();
 
     }
-    
+
 });
 
 // This function clears the list for the high scores and generates a new high score list from local storage
-function generateHighscores(){
+function generateHighscores() {
     highscoreDisplayName.innerHTML = "";
     highscoreDisplayScore.innerHTML = "";
     var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-    for (i=0; i<highscores.length; i++){
+    for (i = 0; i < highscores.length; i++) {
         var newNameSpan = document.createElement("li");
         var newScoreSpan = document.createElement("li");
         newNameSpan.textContent = highscores[i].name;
@@ -152,8 +154,8 @@ function generateHighscores(){
     }
 }
 
-// This function displays the high scores page while hiding all of the other pages from 
-function showHighscore(){
+// display high scores 
+function showHighscore() {
     startQuizDiv.style.display = "none"
     gameoverDiv.style.display = "none";
     highscoreContainer.style.display = "flex";
@@ -163,15 +165,15 @@ function showHighscore(){
     generateHighscores();
 }
 
-// This function clears the local storage of the high scores as well as clearing the text from the high score board
-function clearScore(){
+
+function clearScore() {
     window.localStorage.clear();
     highscoreDisplayName.textContent = "";
     highscoreDisplayScore.textContent = "";
 }
 
-// This function sets all the variables back to their original values and shows the home page to enable replay of the quiz
-function replayQuiz(){
+// reset the quiz
+function replayQuiz() {
     highscoreContainer.style.display = "none";
     gameoverDiv.style.display = "none";
     startQuizDiv.style.display = "flex";
@@ -180,27 +182,28 @@ function replayQuiz(){
     currentQuestionIndex = 0;
 }
 
-// This function checks the response to each answer 
-function checkAnswer(answer){
+// verify the answer to see if its correct
+function checkAnswer(answer) {
     correct = quizQuestions[currentQuestionIndex].correctAnswer;
 
-    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex) {
         score++;
-        alert("!WOW! U GOT IT.");
+        alert("!Great! U GOT IT.");
         currentQuestionIndex++;
         generateQuizQuestion();
-        //display in the results div that the answer is correct.
-    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
-        
+
+       
+    } else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex) {
+
         alert("Ooops, sorry try again.")
-       startTime -=30;
+        startTime -= 30;
         currentQuestionIndex++;
         generateQuizQuestion();
-        //display in the results div that the answer is wrong.
-    }else{
+     
+    } else {
         showScore();
     }
 }
 
-// This button starts the quiz!
-startQuizButton.addEventListener("click",startQuiz);
+// start quiz
+startQuizButton.addEventListener("click", startQuiz);
